c7c4dd8e272c964e523dcd83792284121f08ee2a
dcampuzano101 <dcampuzano101@gmail.com>
2020-10-19 21:54:35 -0400

    talkRoutes

--- backend/routes/talkRoutes.js
+++ backend/routes/talkRoutes.js
@@ -8,7 +8,7 @@ import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(protect, createTalk);
-router.route("/:id").put(protect, addAttendee);
+router.route("/:id/add_attendee").put(protect, addAttendee);
 router.route("/:id/remove_attendee").put(protect, removeAttendee);
 
 export default router;
